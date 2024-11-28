b8e0eac6d5279cc1b3dded9d6d0da8f987ff95aa
enry74 <enri74@yahoo.com>
2024-11-28 13:37:31 +0100

    first commit

---
+++ usr/lib/enigma2/python/Plugins/Extensions/Wireguard/CheckDeps.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+opkg update
+if ! python -c "import requests" &> /dev/null; then
+  echo "Installing python3-requests..."
+  opkg install python3-requests
+fi
+echo "Checking WireGuard dependencies..."
+if [ ! -f /etc/iptables/iptables.rules ]; then
+  echo "Installing iptables..."
+  opkg --force-reinstall --force-overwrite install iptables &> /dev/null
+fi
+if [ ! -f /usr/bin/wg ]; then
+  echo "Installing WireGuard tools and dependencies..."
+  opkg --force-reinstall --force-overwrite install wireguard-tools wireguard-tools-bash-completion openresolv &> /dev/null
+fi
+if [ ! -f /usr/bin/aplay ]; then
+  echo "Installing aplay (alsa-utils)..."
+  opkg --force-reinstall --force-overwrite install alsa-utils &> /dev/null
+fi
+rm -rf /tmp/*.ipk
+exit 0
